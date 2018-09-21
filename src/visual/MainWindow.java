d74b41e7ba57af8f71560fea2947da243b435913
Zerfalex <zerfalex@gmail.com>
2018-09-21 13:46:25 +0100

    Re-organize packages
    Windows and visual stuff now in visual package.

---
+++ src/visual/MainWindow.java
@@ -0,0 +1,57 @@
+package visual;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MainWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewBoy = new JButton("New Boy");
+		btnNewBoy.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				NewStudent ns = new NewStudent();
+				ns.newWindow();
+			}
+		});
+		btnNewBoy.setBounds(154, 147, 117, 25);
+		frame.getContentPane().add(btnNewBoy);
+	}
+
+}
---
+++ src/visual/NewStudent.java
@@ -0,0 +1,43 @@
+package visual;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class NewStudent {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public void newWindow() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					NewStudent window = new NewStudent();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public NewStudent() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
